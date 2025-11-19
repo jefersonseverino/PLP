abd3a500779d08e479c7918bab7c82d4e2d39992
Tales Cunha <tvac@cin.ufpe.br>
2025-11-19 20:21:03 -0300

    feat: implement timezone conversion in TimeStamp using java.time

--- PLP/Imperativa2/src/li2/plp/expressions1/util/TimeStamp.java
+++ PLP/Imperativa2/src/li2/plp/expressions1/util/TimeStamp.java
@@ -1,11 +1,10 @@
 package li2.plp.expressions1.util;
 
-import java.security.Timestamp;
-import java.util.Optional;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
 
 public class TimeStamp {
 
-    /* Date area */
     public Integer year;
     public Integer month;
     public Integer day;
@@ -13,37 +12,35 @@ public class TimeStamp {
     public Integer minute;
     public Integer second;
 
-    /* Timezone area */
     public String tz_str;
     public String tz_signal;
     public Integer tz_hour;
     public Integer tz_minute;
 
     public TimeStamp(Integer year, Integer month, Integer day, Integer hour, Integer minute, Integer second) {
-        System.out.println("CAllig constructor number one");
-
         this.year = year;
         this.month = month;
         this.day = day;
         this.hour = hour;
         this.minute = minute;
         this.second = second;
+        this.tz_str = "UTC";
     }
 
     public TimeStamp(Integer year, Integer month, Integer day, Integer hour, Integer minute, Integer second, String tz_str) {
-        System.out.println("CAllig constructor number two");
         this.year = year;
         this.month = month;
         this.day = day;
         this.hour = hour;
         this.minute = minute;
         this.second = second;
-        this.tz_str = tz_str;
+        
+        String cleanTz = tz_str.replaceAll("\"", "");
+        this.tz_str = cleanTz;
     }
  
     public TimeStamp(Integer year, Integer month, Integer day, Integer hour, Integer minute, Integer second,
             String tz_signal, Integer tz_hour, Integer tz_minute) {
-        System.out.println("CAllig constructor number three");
         this.year = year;
         this.month = month;
         this.day = day;
@@ -53,5 +50,45 @@ public class TimeStamp {
         this.tz_signal = tz_signal;
         this.tz_hour = tz_hour;
         this.tz_minute = tz_minute;
+        
+        String sign = tz_signal.equals("+") ? "+" : "-";
+        this.tz_str = String.format("%s%02d:%02d", sign, tz_hour, tz_minute);
+    }
+
+    public ZoneId getZoneId() {
+        if (tz_str != null) {
+            return TimezoneRegistry.toZoneId(tz_str);
+        }
+        if (tz_signal != null && tz_hour != null && tz_minute != null) {
+            String offsetStr = String.format("%s%02d:%02d", 
+                tz_signal.equals("+") ? "+" : "-", tz_hour, tz_minute);
+            return ZoneId.of(offsetStr);
+        }
+        return ZoneId.of("UTC");
+    }
+
+    public ZonedDateTime toZonedDateTime() {
+        ZoneId zone = getZoneId();
+        return ZonedDateTime.of(year, month, day, hour, minute, second, 0, zone);
+    }
+
+    public TimeStamp convertToTimezone(String targetTimezone) {
+        if (!TimezoneRegistry.isTimezoneSupported(targetTimezone)) {
+            throw new RuntimeException("Unsupported timezone: " + targetTimezone);
+        }
+
+        ZoneId targetZone = TimezoneRegistry.toZoneId(targetTimezone);
+        ZonedDateTime current = toZonedDateTime();
+        ZonedDateTime converted = current.withZoneSameInstant(targetZone);
+
+        return new TimeStamp(
+            converted.getYear(),
+            converted.getMonthValue(),
+            converted.getDayOfMonth(),
+            converted.getHour(),
+            converted.getMinute(),
+            converted.getSecond(),
+            targetTimezone
+        );
     }
 }
