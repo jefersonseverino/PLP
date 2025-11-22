73171a293f71e1380dc069ad3098b8e79d3d5e65
jefersonseverino <jsa2@cin.ufpe.br>
2025-11-22 15:49:13 -0300

    Handling timezone in parse operation

--- Imperativa2/src/li2/plp/expressions2/expression/ExpParser.java
+++ Imperativa2/src/li2/plp/expressions2/expression/ExpParser.java
@@ -18,19 +18,35 @@ public class ExpParser extends ExpUnaria {
     public Valor avaliar(AmbienteExecucao amb) throws VariavelJaDeclaradaException, 
             VariavelNaoDeclaradaException {
         String str = ((ValorString) getExp().avaliar(amb)).valor();
-        String[] parts = str.split("T");
+        String[] parts = str.split("T", 2);
         String datePart = parts[0];
         String timePart = parts[1];
 
-        String[] dateComponents = datePart.split("-");
+        String[] dateComponents = datePart.split("-", 3);
         Integer year = Integer.parseInt(dateComponents[0]);
         Integer month = Integer.parseInt(dateComponents[1]);
         Integer day = Integer.parseInt(dateComponents[2]);
 
-        String[] timeComponents =  timePart.split(":");
+        String[] timeComponents =  timePart.split(":", 3);
         Integer hour = Integer.parseInt(timeComponents[0]);
         Integer minute = Integer.parseInt(timeComponents[1]);
-        Integer second = Integer.parseInt(timeComponents[2]);
+        Integer second = Integer.parseInt(timeComponents[2].substring(0, 2));
+
+        if (timeComponents[2].length() > 2) { // has timezone
+            String timezone = timeComponents[2].substring(2, timeComponents[2].length()).trim();
+            if (timezone.contains("+") || timezone.contains("-")) {
+                String signal = timezone.substring(0, 1);
+                String[] tzComponents = timezone.substring(1).split(":");
+                Integer tzHour = Integer.parseInt(tzComponents[0]);
+                Integer tzMinute = Integer.parseInt(tzComponents[1]);
+
+                TimeStamp ts = new TimeStamp(year, month, day, hour, minute, second, signal, tzHour, tzMinute);
+                return new ValorTimestamp(ts);
+            } else {
+                TimeStamp ts = new TimeStamp(year, month, day, hour, minute, second, timezone);
+                return new ValorTimestamp(ts);
+            }
+        }
 
         TimeStamp ts = new TimeStamp(year, month, day, hour, minute, second);
         return new ValorTimestamp(ts);
